2eb1c5cb7f1b14980425f746aa9c5cad5fa8c2ee
WENG, CHIA-WEN <chiawenweng0114@gmail.com>
2024-06-07 00:57:14 +0800

    Update caesar.py

--- SC001_Assignment3/caesar.py
+++ SC001_Assignment3/caesar.py
@@ -8,39 +8,59 @@ ALPHABET as the cipher table. After that, any strings typed
 in will be encrypted.
 """
 
-
 # This constant shows the original order of alphabetic sequence.
 ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 
 def main():
     """
-    This program demonstrates the idea of caesar cipher.
+    This program helps users to decipher a secret sequence.
+    Users need to input a number that helps the alphabet "shifts" into a different order.
+    Then, when users enter the secret sequence, this program can translate
+    the ciphered string into a deciphered string.
     """
-    secret_number = int(input("Secret number: "))
-    ciphered_string = input("What’s the ciphered string? ")
-    deciphered_string = decipher(ciphered_string, secret_number)
-    print("The deciphered string is:", deciphered_string)
+    secret_num = int(input('Secret number: '))
+    ciphered_string = input('What\'s the ciphered string? ')
+
+    # Case-insensitive: reassign the ciphered string by making it to the upper-case one
+    ciphered_string = ciphered_string.upper()
+
+    ans = decode(secret_num, ciphered_string)
+    print('The deciphered string is: ' + ans)
 
 
-def decipher(ciphered_string, secret_number):
-    new_alphabet = ''
-    new_alphabet = ALPHABET[26-secret_number:] + ALPHABET[:26-secret_number]
-    result = ''
-    for i in range(len(ciphered_string)):
-        ch = new_alphabet.find(ciphered_string[i].upper())
-        if ciphered_string[i].isalpha():
-            result = result + ALPHABET[ch]
-        else:
-            result = result + ciphered_string[i]
-    return result
+def decode(num, input_text):
+    """
+    :param num: int, the number that is used to offset the original order in the sequence.
+    :param input_text: str, the test to be deciphered.
+    :return: str, the deciphered texts.
+    """
+    new_seq = new_sequence(num)
+    ans = ''
+
+    for ch in input_text:
+
+        if new_seq.find(ch) != -1:      # When the character is not a space or a symbol,
+                                        # shift the character back to the original one
+                                        # and add to ans.
+            ans += ALPHABET[new_seq.find(ch)]
+
+        else:                           # Add whatever string that is not an alphabet.
+            ans += ch
+
+    return ans
 
 
+def new_sequence(num):
+    """
+    :param num: int, the number that is used to offset the original order in the sequence.
+    :return: str, the "shifted" version of the ALPHABET.
+    """
+    first_half = ALPHABET[:len(ALPHABET)-num]
+    second_half = ALPHABET[len(ALPHABET)-num:]
 
+    return second_half + first_half
 
-
-
-# DO NOT EDIT CODE BELOW THIS LINE #
 
 if __name__ == '__main__':
     main()
