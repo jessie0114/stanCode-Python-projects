75179468a0af25b4739e2d2bd90c2ec221e93086
WENG, CHIA-WEN <chiawenweng0114@gmail.com>
2024-06-08 00:18:46 +0800

    Update fire.py

--- SC001_Assignment4/fire.py
+++ SC001_Assignment4/fire.py
@@ -15,34 +15,37 @@ HURDLE_FACTOR = 1.05
 
 def highlight_fires(filename):
     """
-    :param filename:str, the file path of the original image (with respect to current directory)
-    :return:SimpleImage, the image with highlight fires and make the rest pixel gray.
+    :param filename: str, Import image path
+    :return img: SimpleImage, Mark the picture of the fire area
+    Function: Mark fire area
+    Principle: Use mean value comparison to find the area with high R value and mark it out
     """
-    highlight_img = SimpleImage(filename)
-    for x in range(highlight_img.width):
-        for y in range(highlight_img.height):
-            pixel = highlight_img.get_pixel(x, y)
-            avg = (pixel.red+pixel.blue+pixel.green) // 3
-            if pixel.red > avg * HURDLE_FACTOR
+    img = SimpleImage(filename)
+    for y in range(img.height):
+        for x in range(img.width):
+            pixel = img.get_pixel(x, y)
+            grayscale = (pixel.red + pixel.green + pixel.blue) // 3
+            if pixel.red > grayscale * HURDLE_FACTOR:
                 pixel.red = 255
+                pixel.green = 0
+                pixel.blue = 0
             else:
-                pixel.red = avg
-                pixel.green = avg
-                pixel.blue = avg
-    return 0
+                pixel.red = grayscale
+                pixel.green = grayscale
+                pixel.blue = grayscale
+    return img
 
 
 def main():
     """
-    This program will highlight the fire and make the rest pixel gray in original picture.
+    Function: Mark fire area
+    Principle: Display the original picture and the special fire area picture
     """
     original_fire = SimpleImage('images/greenland-fire.png')
     original_fire.show()
     highlighted_fire = highlight_fires('images/greenland-fire.png')
     highlighted_fire.show()
-
-
-# DO NOT EDIT CODE BELOW THIS LINE #
+    
 
 if __name__ == '__main__':
     main()
